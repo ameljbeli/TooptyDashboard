d3e980d7503615670e4c5f69b205f00bbc893ab1
ameljbeli <amel.jbeli@yahoo.fr>
2018-11-04 11:48:44 +0100

    Ajoutez des fichiers projet.

---
+++ TooptyDashboard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TooptyDashboard.Startup))]
+namespace TooptyDashboard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
