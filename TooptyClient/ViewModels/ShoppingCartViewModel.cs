2aef224de4015901e047f7284964471ec7f00783
ameljbeli <amel.jbeli@yahoo.fr>
2018-11-24 21:10:19 +0100

    Shoppingcat

---
+++ TooptyClient/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using TooptyClient.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TooptyClient.ViewModel
+{
+    public class ShoppingCartViewModel
+    {
+        [Key]
+        public List<ShoppingCartItem> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
